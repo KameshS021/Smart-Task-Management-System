eb1a28ead6dd79a67bdebe85c4b3a3d5ff81f582
Kameshjustin <kameshjustin11@gmail.com>
2026-07-28 17:44:58 +0530

    Update frontend API base URL to API Gateway ingress

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://api.smarttask.local:31358",
   headers: {
     "Content-Type": "application/json",
   },
