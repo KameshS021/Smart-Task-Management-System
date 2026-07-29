9e6662cb17a59acc6d57aa0f8d955d9390b0c0e7
Kameshjustin <kameshjustin11@gmail.com>
2026-07-29 11:40:35 +0530

    Add Prometheus metrics endpoint to Api Gateway

--- api-gateway_1784010924579/src/app.js
+++ api-gateway_1784010924579/src/app.js
@@ -7,6 +7,9 @@ const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const proxy = require("express-http-proxy");
 
+// Prometheus
+const { client, httpRequestCounter } = require("./metrics");
+
 const app = express();
 
 app.use(express.json());
@@ -16,25 +19,43 @@ app.use(morgan("dev"));
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100
+  max: 100,
 });
 
 app.use(limiter);
+
+// Prometheus HTTP Request Counter
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    httpRequestCounter.inc({
+      method: req.method,
+      route: req.path,
+      status: res.statusCode,
+    });
+  });
+
+  next();
+});
+
+// Prometheus Metrics Endpoint
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
 
 // Health Check
 app.get("/health", (req, res) => {
   res.json({
     success: true,
     service: "API Gateway",
-    status: "Running"
+    status: "Running",
   });
 });
 
 // Auth Service Proxy
 app.use(
   "/api/auth",
-  proxy(process.env.AUTH_SERVICE_URL,
-{
+  proxy(process.env.AUTH_SERVICE_URL, {
     proxyReqPathResolver: (req) => {
       return "/api/auth" + req.url;
     },
@@ -44,8 +65,7 @@ app.use(
 // Task Service Proxy
 app.use(
   "/api/tasks",
-  proxy(process.env.TASK_SERVICE_URL,
-{
+  proxy(process.env.TASK_SERVICE_URL, {
     proxyReqPathResolver: (req) => {
       return "/api/tasks" + req.url;
     },
@@ -53,6 +73,3 @@ app.use(
 );
 
 module.exports = app;
-
-
-
